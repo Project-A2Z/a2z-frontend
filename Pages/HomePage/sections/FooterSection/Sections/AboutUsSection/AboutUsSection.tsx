e375ebb2f32e6d18639db7b1cfeb3d4c76c55b04
NesreenAbotaleb <mohamedaboelhawey@gmail.com>
2025-09-05 16:34:22 +0300

    footer without bg

--- Pages/HomePage/sections/FooterSection/Sections/AboutUsSection/AboutUsSection.tsx
+++ Pages/HomePage/sections/FooterSection/Sections/AboutUsSection/AboutUsSection.tsx
@@ -3,15 +3,15 @@ import LogoSection from "@/Pages/AuthPages/ActiveCodePage/sections/LogoSection/L
 
 const AboutUsSection = () => {
     return (
-      <div className="w-full sm:w-[80%] md:w-[60%] lg:w-[16%] min-h-[200px] sm:min-h-[180px] md:min-h-[160px] lg:h-[20vh] flex flex-col justify-end items-end bg-blue-600">
-        <div className="w-full sm:w-[90%] md:w-[85%] lg:w-[80%] min-h-[8vh] sm:min-h-[7vh] md:min-h-[6vh] flex flex-row justify-between items-center text-center gap-2 sm:gap-3 md:gap-4 opacity-100 bg-amber-800 mx-auto sm:ml-8 md:ml-10 lg:ml-11 px-2 sm:px-3 md:px-4 lg:px-0">
+      <div className="w-full sm:w-[80%] md:w-[60%] lg:w-[16%] min-h-[200px] sm:min-h-[180px] md:min-h-[160px] lg:h-[20vh] flex flex-col justify-end items-end ">
+        <div className="w-full sm:w-[90%] md:w-[85%] lg:w-[80%] min-h-[8vh] sm:min-h-[7vh] md:min-h-[6vh] flex flex-row justify-between items-center text-center gap-2 sm:gap-3 md:gap-4 opacity-100  mx-auto sm:ml-8 md:ml-10 lg:ml-11 px-2 sm:px-3 md:px-4 lg:px-0">
           {/* company name - positioned on the left */}
-          <div className="w-[35%] sm:w-[40%] md:w-[45%] lg:w-[60%] min-h-[6vh] sm:min-h-[5.5vh] md:min-h-[5vh] bg-wight opacity-100 flex justify-start items-center bg-red-500 order-1">
+          <div className="w-[35%] sm:w-[40%] md:w-[45%] lg:w-[60%] min-h-[6vh] sm:min-h-[5.5vh] md:min-h-[5vh] bg-wight opacity-100 flex justify-start items-center order-1">
             <span className="font-beiruti font-semibold text-lg sm:text-xl md:text-2xl leading-none text-black87 text-left ">شركة A2Z</span>
           </div>
           
           {/* logo - positioned on the right */}
-          <div className="w-[35%] sm:w-[40%] md:w-[45%] lg:w-[50%] h-[4vh] sm:h-[4.5vh] md:h-[5vh] lg:h-[100%] opacity-100 rotate-0 bg-slate-600 order-2">
+          <div className="w-[35%] sm:w-[40%] md:w-[45%] lg:w-[50%] h-[4vh] sm:h-[4.5vh] md:h-[5vh] lg:h-[100%] opacity-100 rotate-0  order-2">
             <img
             src="/acessts/Logo-picsart.png"
             alt="A2Z Logo"
