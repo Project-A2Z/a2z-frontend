5e61731162a79c86072a83c61adc62a6b30c1dfa
ahmedsaed7 <ahmedsaedabdallsalam614@gmail.com>
2025-09-20 15:55:12 +0300

    Update the last items in the footer

--- Pages/HomePage/sections/FooterSection/Sections/AboutUsSection/AboutUsSection.tsx
+++ Pages/HomePage/sections/FooterSection/Sections/AboutUsSection/AboutUsSection.tsx
@@ -3,21 +3,21 @@ import LogoSection from "@/Pages/AuthPages/ActiveCodePage/sections/LogoSection/L
 
 const AboutUsSection = () => {
     return (
-      <div className="w-full sm:w-[80%] md:w-[60%] lg:w-[16%] min-h-[200px] sm:min-h-[180px] md:min-h-[160px] lg:h-[20vh] flex flex-col justify-end items-end ">
-        <div className="w-full sm:w-[90%] md:w-[85%] lg:w-[80%] min-h-[8vh] sm:min-h-[7vh] md:min-h-[6vh] flex flex-row justify-between items-center text-center gap-2 sm:gap-3 md:gap-4 opacity-100  mx-auto sm:ml-8 md:ml-10 lg:ml-11 px-2 sm:px-3 md:px-4 lg:px-0">
+      <div className="w-full sm:w-[80%] md:w-[60%] lg:w-[16%] min-h-[180px] sm:min-h-[160px] md:min-h-[140px] lg:h-[18vh] flex flex-col justify-end items-end ">
+        <div className="w-full sm:w-[95%] md:w-[90%] lg:w-[85%] min-h-[7vh] sm:min-h-[6vh] md:min-h-[5vh] flex flex-row  items-center gap-1 sm:gap-2 md:gap-3 mx-auto px-1 sm:px-2">
           {/* company name - positioned on the left */}
-          <div className="w-[35%] sm:w-[40%] md:w-[45%] lg:w-[60%] min-h-[6vh] sm:min-h-[5.5vh] md:min-h-[5vh] bg-wight opacity-100 flex justify-start items-center order-1">
-            <span className="font-beiruti font-semibold text-lg sm:text-xl md:text-2xl leading-none text-black87 text-left ">شركة A2Z</span>
+          <div className="w-[45%] sm:w-[40%] md:w-[50%] lg:w-[55%] min-h-[5vh] sm:min-h-[4.5vh] flex justify-start items-center ">
+            <span className="font-beiruti font-semibold text-base sm:text-lg md:text-xl lg:text-2xl text-black87">شركة A2Z</span>
           </div>
           
           {/* logo - positioned on the right */}
-          <div className="w-[35%] sm:w-[40%] md:w-[45%] lg:w-[50%] h-[4vh] sm:h-[4.5vh] md:h-[5vh] lg:h-[100%] opacity-100 rotate-0  order-2">
+          <div className="w-[40%] sm:w-[30%] md:w-[40%]   lg:w-[40%] h-[4vh] sm:h-[4.5vh] md:h-[5vh] lg:h-[90%] flex  ">
             <img
             src="/acessts/Logo-picsart.png"
             alt="A2Z Logo"
-            className="w-full h-full object-contain"
+            className="w-full h-full object-contain "
             />
-			    </div>
+          </div>
         </div>
         <p className="w-full h-auto lg:h-[70%] opacity-100 rotate-0 text-sm sm:text-base leading-relaxed sm:leading-none tracking-normal text-right font-medium font-beiruti pt-4 sm:pt-6 md:pt-7 lg:pt-8 px-2 sm:px-3 md:px-4 lg:px-0">
           شركة متخصصة في جميع انواع الكيماويات
