bb6f2ab3db6c8baca54e5e0fb1576bb8cdbea180
ahmedsaed7 <ahmedsaedabdallsalam614@gmail.com>
2025-09-30 17:15:19 +0300

    the connection of product , contact, navtowathapp, cart , fav , forgetpassword

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   webpack(config) {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**', // Allow all paths under Cloudinary
+      },
+    ],
+    formats: ['image/webp', 'image/avif'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
+  },
+  webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"]
     });
     return config;
-  }
+  },
+  experimental: {},
+  typescript: {},
+  env: {},
 };
 
 export default nextConfig;
